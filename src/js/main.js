55a3ba7ee5756d2e10da037ec2a21bdb218b0513
Flavio Alfonzetti <flavioalfonzetti@gmail.com>
2023-11-13 02:17:31 +0100

    build dist folder

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,15 @@ import "../scss/styles.scss";
 const form = document.getElementById('form')
 const emailInput = document.getElementById('email-input');
 const error = document.getElementById('error');
-const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // Simple email pattern
+const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
 form.addEventListener('submit', function (e) {
-  // Always get the current value of the email field when the form is submitted
   const email = emailInput.value;
 
   if (!regex.test(email)) {
-    // If the email doesn't match the pattern, show error
     error.style.display = 'inline';
     emailInput.style.borderColor = 'var(--primary-light-red)';
-    e.preventDefault(); // Prevent form from submitting
+    e.preventDefault();
   }
 });
 
